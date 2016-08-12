7fc3dd03d1de1228f56efe2af27b404a5f35617c
tfx2001 <2479727366@qq.com>
2016-08-12 16:31:13 +0800

    更新

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
 // Empty JS for your own code to be here
+$(".lanren ul li").hover(function(){
+	$(this).find(".txt").stop().animate({height:"198px"},400);
+	$(this).find(".txt h3").stop().animate({paddingTop:"60px"},400);
+},function(){
+	$(this).find(".txt").stop().animate({height:"45px"},400);
+	$(this).find(".txt h3").stop().animate({paddingTop:"0px"},400);
+})
