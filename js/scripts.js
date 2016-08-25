6c5a7bdc58ed309a32ef60108f5e1ed4db7e2092
tfx2001 <2479727366@qq.com>
2016-08-25 22:04:28 +0800

    debug

--- js/scripts.js
+++ js/scripts.js
@@ -8,10 +8,10 @@ $(".lanren ul li").hover(function(){
 })
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) )
 {
-	var s = "<a class=\"btn btn-default\" href=\"https://www.bilibili.com/video/av5855156/\" role=\"button\">av5855156</a>";
+	var s = "<a class=\"btn btn-default\" href=\"http://www.bilibili.com/video/av5855156/\" role=\"button\">av5855156</a>";
 	document.getElementById("p1").innerHTML += s
 }
 else{
-	var s = "<embed height=\"415\" width=\"544\" quality=\"high\" allowfullscreen=\"true\" type=\"application/x-shockwave-flash\" src=\"http://static.hdslb.com/miniloader.swf\" flashvars=\"aid=5855156&page=1\" pluginspage=\"http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash\"></embed>";
+	var s = "<embed height=\"415\" width=\"544\" quality=\"high\" allowfullscreen=\"true\" type=\"application/x-shockwave-flash\" src=\"https://static.hdslb.com/miniloader.swf\" flashvars=\"aid=5855156&page=1\" pluginspage=\"https://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash\"></embed>";
 	document.getElementById("p1").innerHTML += s
 }
