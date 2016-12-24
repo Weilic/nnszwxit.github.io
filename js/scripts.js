393b2f851ced0eb834dcfd76641bd79063e89dec
tfx2001 <2479727366@qq.com>
2016-12-25 00:13:06 +0800

    debug

--- js/scripts.js
+++ js/scripts.js
@@ -9,20 +9,20 @@ $(".lanren ul li").hover(function(){
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) )
 {
 	var s = "<a class=\"btn btn-default\" href=\"http://www.bilibili.com/video/av5855156/\" role=\"button\">av5855156</a>";
-	document.getElementById("p1").innerHTML += s
+	document.getElementById("p1").innerHTML += s;
 }
 else{
 	var s = "<embed height=\"415\" width=\"544\" quality=\"high\" allowfullscreen=\"true\" type=\"application/x-shockwave-flash\" src=\"https://static.hdslb.com/miniloader.swf\" flashvars=\"aid=5855156&page=1\" pluginspage=\"https://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash\"></embed>";
-	document.getElementById("p1").innerHTML += s
+	document.getElementById("p1").innerHTML += s;
 }
 function time() 
 { 
-	 var theTime="2017/11/11" 
+	 var theTime="2017/11/11" ;
 	 var endTime=new Date(theTime); 
 	 var totalSecs=(endTime-new Date())/1000; 
 	 var days=Math.floor(totalSecs/3600/24); 
 	 document.getElementById("time1").innerHTML="距离NOIp2017 还有<strong>"+days+"天</strong>"; 
-	 var theTime="2017/8/1" 
+	 var theTime="2017/8/1" ;
 	 var endTime=new Date(theTime); 
 	 var totalSecs=(endTime-new Date())/1000; 
 	 var days=Math.floor(totalSecs/3600/24); 
