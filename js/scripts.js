b2f0828db87a070144230c872620ee85ddd9fe1e
tfx2001 <2479727366@qq.com>
2017-02-11 13:09:18 +0800

    本次更新内容
    1.更新了多说在线评论系统
    2.去掉了【喜庆的背景】
    3.加入了显示页面更新时间

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,40 @@
 // Empty JS for your own code to be here
-$(".lanren ul li").hover(function(){
-	$(this).find(".txt").stop().animate({height:"198px"},400);
-	$(this).find(".txt h3").stop().animate({paddingTop:"60px"},400);
-},function(){
-	$(this).find(".txt").stop().animate({height:"45px"},400);
-	$(this).find(".txt h3").stop().animate({paddingTop:"0px"},400);
+$(".lanren ul li").hover(function () {
+	$(this).find(".txt").stop().animate({
+		height: "198px"
+	}, 400);
+	$(this).find(".txt h3").stop().animate({
+		paddingTop: "60px"
+	}, 400);
+}, function () {
+	$(this).find(".txt").stop().animate({
+		height: "45px"
+	}, 400);
+	$(this).find(".txt h3").stop().animate({
+		paddingTop: "0px"
+	}, 400);
 })
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) )
-{
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
 	var s = "<a class=\"btn btn-default\" href=\"http://www.bilibili.com/video/av5855156/\" role=\"button\">av5855156</a>";
-	document.getElementById("p1").innerHTML += s;
-}
-else{
+	document.getElementById("p1").innerHTML = s;
+} else {
 	var s = "<embed height=\"415\" width=\"544\" quality=\"high\" allowfullscreen=\"true\" type=\"application/x-shockwave-flash\" src=\"https://static.hdslb.com/miniloader.swf\" flashvars=\"aid=5855156&page=1\" pluginspage=\"https://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash\"></embed>";
-	document.getElementById("p1").innerHTML += s;
+	document.getElementById("p1").innerHTML = s;
 }
-function time() 
-{ 
-	 var theTime="2017/11/11" ;
-	 var endTime=new Date(theTime); 
-	 var totalSecs=(endTime-new Date())/1000; 
-	 var days=Math.floor(totalSecs/3600/24); 
-	 document.getElementById("time1").innerHTML="距离NOIp2017 还有<strong>"+days+"天</strong>"; 
-	 var theTime="2017/8/1" ;
-	 var endTime=new Date(theTime); 
-	 var totalSecs=(endTime-new Date())/1000; 
-	 var days=Math.floor(totalSecs/3600/24); 
-	 document.getElementById("time2").innerHTML="距离 NOI2017 还有<strong>"+days+"天</strong>"; 
-} 
-window.onload=function() 
-{ 
+document.getElementById("lastUpdate").innerHTML = "本页面最后修改于" + document.lastModified;
+
+function time() {
+	var theTime = "2017/11/11";
+	var endTime = new Date(theTime);
+	var totalSecs = (endTime - new Date()) / 1000;
+	var days = Math.floor(totalSecs / 3600 / 24);
+	document.getElementById("time1").innerHTML = "距离NOIp2017 还有<strong>" + days + "天</strong>";
+	var theTime = "2017/8/1";
+	var endTime = new Date(theTime);
+	var totalSecs = (endTime - new Date()) / 1000;
+	var days = Math.floor(totalSecs / 3600 / 24);
+	document.getElementById("time2").innerHTML = "距离 NOI2017 还有<strong>" + days + "天</strong>";
+}
+window.onload = function () {
 	time();
-} 
+}
