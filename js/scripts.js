3d969fe86a34cc49f13fe3926f2c4238b2a7c4c5
tfx2001 <2479727366@qq.com>
2017-07-22 14:00:20 +0800

    重写网页模板

--- js/scripts.js
+++ js/scripts.js
@@ -14,14 +14,6 @@ $(".lanren ul li").hover(function () {
 		paddingTop: "0px"
 	}, 400);
 })
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-	var s = "<a class=\"btn btn-default\" href=\"http://www.bilibili.com/video/av5855156/\" role=\"button\">av5855156</a>";
-	document.getElementById("p1").innerHTML = s;
-} else {
-	var s = "<embed height=\"415\" width=\"544\" quality=\"high\" allowfullscreen=\"true\" type=\"application/x-shockwave-flash\" src=\"https://static.hdslb.com/miniloader.swf\" flashvars=\"aid=5855156&page=1\" pluginspage=\"https://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash\"></embed>";
-	document.getElementById("p1").innerHTML = s;
-}
-document.getElementById("lastUpdate").innerHTML = "本页面最后修改于" + document.lastModified;
 
 function time() {
 	var theTime = "2017/11/11";
@@ -35,6 +27,14 @@ function time() {
 	var days = Math.floor(totalSecs / 3600 / 24);
 	document.getElementById("time2").innerHTML = "距离 NOI2017 还有<strong>" + days + "天</strong>";
 }
-window.onload = function () {
-	//time();
+
+window.onload = new function () {
+	document.getElementById("lastUpdate").innerHTML = "本页面最后修改于" + document.lastModified;
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+		var s = "<a class=\"btn btn-default\" href=\"http://www.bilibili.com/video/av5855156/\" role=\"button\">av5855156</a>";
+		document.getElementById("p1").innerHTML = s;
+	} else {
+		var s = "<embed height=\"415\" width=\"544\" quality=\"high\" allowfullscreen=\"true\" type=\"application/x-shockwave-flash\" src=\"https://static.hdslb.com/miniloader.swf\" flashvars=\"aid=5855156&page=1\" pluginspage=\"https://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash\"></embed>";
+		document.getElementById("p1").innerHTML = s;
+	}
 }
