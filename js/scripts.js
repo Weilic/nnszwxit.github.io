b1176d2eaeddc1d9a37c7734cc368bf384278ea0
tfx2001 <2479727366@qq.com>
2017-07-24 21:58:47 +0800

    Bug Fix

--- js/scripts.js
+++ js/scripts.js
@@ -33,11 +33,4 @@ function verticalAlign(){
 window.onload = new function () {
 	document.getElementById("lastUpdate").innerHTML = "本页面最后修改于" + document.lastModified;
 	verticalAlign();
-	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-		var s = "<a class=\"btn btn-default\" href=\"http://www.bilibili.com/video/av5855156/\" role=\"button\">av5855156</a>";
-		document.getElementById("p1").innerHTML = s;
-	} else {
-		var s = "<embed height=\"415\" width=\"544\" quality=\"high\" allowfullscreen=\"true\" type=\"application/x-shockwave-flash\" src=\"https://static.hdslb.com/miniloader.swf\" flashvars=\"aid=5855156&page=1\" pluginspage=\"https://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash\"></embed>";
-		document.getElementById("p1").innerHTML = s;
-	}
 }
